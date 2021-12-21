738fa8afa6decdb86d1d08e9251330e930786658
Samuel Araújo <70605822+Amelus99@users.noreply.github.com>
2021-12-21 19:53:11 -0300

    Update matar.sh

--- ProjetoShell/matar.sh
+++ ProjetoShell/matar.sh
@@ -1,4 +1,4 @@
-processo=$(yad --title="Matar processo" --center --form --text="Digite o PID do processo que deseja matar:" --field="PID: " --button="OK:0" --button="Cancelar:1" --width=600)
+processo=$(yad --title="Matar processo" --center --form --text="Digite o PID do processo que deseja matar:" --field="PID: " --button="OK:0" --button="Cancelar:1" --width=300)
 kill ${processo:0:-1}
 
 #yad --center --width=500 --height=500 --title="matar processo"
